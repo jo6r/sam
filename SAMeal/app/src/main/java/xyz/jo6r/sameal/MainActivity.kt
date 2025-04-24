c7f71588d6159ff20ef1789ad71b508fd1c42d7d
jo6r <jan.guznar@gmail.com>
2025-04-24 10:51:12 +0200

    update by code analysis

--- SAMeal/app/src/main/java/xyz/jo6r/sameal/MainActivity.kt
+++ SAMeal/app/src/main/java/xyz/jo6r/sameal/MainActivity.kt
@@ -6,7 +6,9 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.OptIn
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -74,6 +76,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalGetImage::class)
 @SuppressLint("ContextCastToActivity")
 @Composable
 fun QRScannerView() {
