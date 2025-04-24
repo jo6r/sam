a7b3eba273805017549a52aed9bba79cbe62c6fb
jo6r <jan.guznar@gmail.com>
2025-04-24 10:47:31 +0200

    update camera libs

--- SAMeal/app/build.gradle.kts
+++ SAMeal/app/build.gradle.kts
@@ -55,11 +55,11 @@ android {
 }
 
 dependencies {
-    implementation("androidx.camera:camera-core:1.3.0")
-    implementation("androidx.camera:camera-camera2:1.3.0")
-    implementation("androidx.camera:camera-lifecycle:1.3.0")
-    implementation("androidx.camera:camera-view:1.3.0")
-    implementation("com.google.mlkit:barcode-scanning:17.2.0")
+    implementation("androidx.camera:camera-core:1.4.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
